4d7875ff722bf923e2cff3db6a61abafee25aebc
mael-boutsoque <mael.boutsoque@gmail.com>
2024-10-16 14:56:54 +0200

    hide hitbox

--- jeu non exe/src/model/PacmanPainter.java
+++ jeu non exe/src/model/PacmanPainter.java
@@ -49,17 +49,16 @@ public class PacmanPainter implements GamePainter {
 			// show hitbox to debbug
 			if(entitee.show_hitbox) {
 				crayon.setColor(Color.BLUE);
+				crayon.drawRect(entitee.hitbox.get_x(), entitee.hitbox.get_y(), entitee.hitbox.get_width(), entitee.hitbox.get_height());
 			}
-			else {
-				crayon.setColor(Color.RED);
-			}
-			crayon.drawRect(entitee.hitbox.get_x(), entitee.hitbox.get_y(), entitee.hitbox.get_width(), entitee.hitbox.get_height());
 			
+			/*
 			try {
 				crayon.setColor(Color.GREEN);
 				crayon.drawRect(entitee.hitboxTemp.get_x(), entitee.hitboxTemp.get_y(), entitee.hitboxTemp.get_width(), entitee.hitboxTemp.get_height());
 			}
 			catch(Exception e){}
+			*/
 				
 		}
 
